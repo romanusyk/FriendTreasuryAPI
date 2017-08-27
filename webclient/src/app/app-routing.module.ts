1ab6e26a891a4253d97e1e1af5f9e0fdea785a00
Yevhenii <yevhenii.stepaniuk@gmail.com>
2017-08-27 11:28:15 +0300

    Added auth component

--- webclient/src/app/app-routing.module.ts
+++ webclient/src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthComponent } from './auth/auth.component';
 
 const routes: Routes = [
   {
-    path: '',
-    children: []
+    path: 'auth',
+    component: AuthComponent
   }
 ];
 
