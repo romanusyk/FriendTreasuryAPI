64c93acf724be9a9597fc306adac0ab454728ccd
YevheniiStepaniuk <yevhenii.stepaniuk@gmail.com>
2017-09-01 15:27:25 +0300

    added more validation

--- webclient/src/app/auth/auth.component.ts
+++ webclient/src/app/auth/auth.component.ts
@@ -23,31 +23,76 @@ export class AuthComponent implements OnInit {
       'password': ['', Validators.required]
     });
   }
- ngOnInit() {
-   this.route.url.subscribe(data => {
-     // Get the last piece of the URL (it's either 'login' or 'register')
-     this.authType = data[data.length - 1].path;
+  ngOnInit() {
+    this.route.url.subscribe(data => {
+      // Get the last piece of the URL (it's either 'login' or 'register')
+      this.authType = data[data.length - 1].path;
       console.log(data);
       // Set a title for the page accordingly
       this.title = (this.authType === 'login') ? 'Sign in' : 'Sign up';
-      this.authForm.addControl('phone', new FormControl(){validators: []});
+      this.authForm.addControl('phone', new FormControl('', [Validators.required, Validators.maxLength(12)]));
       // add form control for username if this is the register page
+      this.authForm.valueChanges.subscribe(this.onValueChange);
     });
   }
   submitForm() {
     this.loading = true;
     this.errors = new Errors();
-
     const credentials = this.authForm.value;
     this.userService
-    .attemptAuth(this.authType, credentials)
-    .subscribe(
+      .attemptAuth(this.authType, credentials)
+      .subscribe(
       data => this.router.navigateByUrl('/'),
       err => {
         this.errors = err;
         this.loading = false;
       }
-    );
+      );
+  }
+
+  onValueChange(data?: any) {
+    console.log(data);
+    for (const key in this.authForm.controls) {
+      if (this.authForm.controls.hasOwnProperty(key)) {
+        const control = this.authForm.get(key);
+        if (control && control.dirty && !control.valid) {
+          const message = ValidationMessages[key];
+              for (const error in control.errors) {
+                if (this.authForm.controls.hasOwnProperty(key)) {
+                  // this.errors.errors()
+              }
+        }
+      }
+    }
+  }
+    // array.forEach(element => {
+    // });
+    // // tslint:disable-next-line:forin
+    // this.authForm.controls.forEach((field: FormControl) => {
+    //   const control = this.authForm.get(field);
+    // });
+
+    //   if (control && control.dirty && !control.valid) {
+    //     const message = this.validationMessages[field];
+    //     for (const key in control.errors) {
+    //       this.formErrors[field] += message[key] + ' ';
+    //     }
+    //   }
+    // }
   }
 
 }
+
+export const ValidationMessages = {
+  'username': {
+    'required': 'Обязательное поле.'
+  },
+  'password': {
+    'required': 'Обязательное поле.'
+  },
+  'phone': {
+    'required': 'Обязательное поле.',
+    'maxlength': 'Обязательное поле.'
+  }
+};
+
