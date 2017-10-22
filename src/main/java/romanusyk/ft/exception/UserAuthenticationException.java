9e20ae75215dd72b5c86586736ca7e4e01aa667f
Romm17 <romanysik@gmail.com>
2017-10-22 13:42:54 +0300

    add BAD_REQUEST

--- src/main/java/romanusyk/ft/exception/UserAuthenticationException.java
+++ src/main/java/romanusyk/ft/exception/UserAuthenticationException.java
@@ -1,8 +1,12 @@
 package romanusyk.ft.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * Created by Roman Usyk on 22.10.17.
  */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class UserAuthenticationException extends RuntimeException {
 
     public UserAuthenticationException() {
