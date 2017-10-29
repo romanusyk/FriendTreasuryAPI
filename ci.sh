02ca03ca0c828b00608228316817ecd792b95072
Romm17 <romanysik@gmail.com>
2017-10-29 19:59:59 +0000

    ci

--- ci.sh
+++ ci.sh
@@ -3,12 +3,13 @@ UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BRANCH="master"
-INTERVAL=$((60 * 5))
+INTERVAL=$((10*3))
 while true;
 do
 	if [ $LOCAL = $REMOTE ]; then
 		echo "Up-to-date"
 	elif [ $LOCAL = $BASE ]; then
+		echo "Deploy"
 		git pull origin $BRANCH 
 		bash start.sh
 		bash webclient/start.sh
