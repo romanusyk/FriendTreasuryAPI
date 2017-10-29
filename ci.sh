6388b6d0a0f642eb6d78a319276e7007004dc583
Yevhenii <yevhenii.stepaniuk@gmail.com>
2017-10-29 16:56:44 +0200

    Added CI

---
+++ ci.sh
@@ -0,0 +1,16 @@
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BRANCH="master"
+INTERVAL=60*5
+while true;
+do
+	if [ $LOCAL = $REMOTE ]; then
+		echo "Up-to-date"
+	elif [ $LOCAL = $BASE ]; then
+		git pull origin $BRANCH 
+		screen -d -m bash start.sh
+		bash ./webclient/start.sh
+	fi;
+sleep $INTERVAL;
+done
