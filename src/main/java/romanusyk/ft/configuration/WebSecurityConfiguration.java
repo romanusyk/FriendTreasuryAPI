4fe84716352e4fd5a8c1e851ec48bab440827bca
Roman Usyk <romanysik@gmail.com>
2017-11-06 16:18:47 +0200

    index.html 404 error

--- src/main/java/romanusyk/ft/configuration/WebSecurityConfiguration.java
+++ src/main/java/romanusyk/ft/configuration/WebSecurityConfiguration.java
@@ -101,12 +101,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers(
-                "/v2/api-docs",
-                "/configuration/ui",
-                "/swagger-resources/**",
-                "/configuration/security",
-                "/swagger-ui.html",
-                "/webjars/**"
+                "/**"
         );
     }
 }
