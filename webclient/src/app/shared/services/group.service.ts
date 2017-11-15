6bbbfd0d07534b5182e2c3ec3893d24226358c81
Yevhenii Stepaniuk <yevhenii.stepaniuk@gmail.com>
2017-11-15 16:25:38 +0200

    Update group.service.ts

--- webclient/src/app/shared/services/group.service.ts
+++ webclient/src/app/shared/services/group.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
-import { forkJoin } from 'rxjs/Observable/forkJoin';
+import 'rxjs/add/observable/forkJoin';
 
 import { PaymentsService } from './payments.service';
 import { PaymentsFilters } from '../models/payments-filters.model';
