9e00825b44de5542667a332f08cf6e4f2109f9d0
Roman Usyk <romanysik@gmail.com>
2017-11-29 00:38:40 +0200

    set max color to 300 and add clipping

--- webclient/src/app/shared/directives/amount-color.directive.ts
+++ webclient/src/app/shared/directives/amount-color.directive.ts
@@ -5,10 +5,12 @@ export class AmountColorDirective implements OnInit {
   @Input('amountColor') amountColor: number;
   private color: string;
   private minPrice = 0;
-  private maxPrice = 250;
+  private maxPrice = 300;
   constructor(private el: ElementRef) { }
   ngOnInit(): void {
-    const coeff = (this.amountColor - this.minPrice) / (this.maxPrice - this.minPrice);
+    let coeff = (this.amountColor - this.minPrice) / (this.maxPrice - this.minPrice);
+    coeff = coeff > 1. ? 1. : coeff;
+    coeff = coeff < 0. ? 0. : coeff;
     this.color = `rgb(${Math.round(Math.sin(coeff * Math.PI / 2) * 255)},${Math.round(Math.cos(coeff * Math.PI / 2) * 255)},0)`;
     (<HTMLElement>this.el.nativeElement).style.color = this.color;
   }
