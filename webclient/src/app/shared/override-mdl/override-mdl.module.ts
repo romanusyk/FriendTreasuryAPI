45725b270ed0e83e236541073a50bed7046db0ab
YevheniiStepaniuk <yevhenii.stepaniuk@gmail.com>
2017-12-22 18:09:58 +0200

    feat: new checkbox

--- webclient/src/app/shared/override-mdl/override-mdl.module.ts
+++ webclient/src/app/shared/override-mdl/override-mdl.module.ts
@@ -1,3 +1,4 @@
+import { FtCheckBoxComponent } from './checkbox/checkbox.component';
 import { FtTextFieldComponent } from './text-field/text-field.component';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -7,7 +8,7 @@ import { NgModule } from '@angular/core';
 
 @NgModule({
     imports: [MdlModule, FormsModule, CommonModule],
-    exports: [FtTextFieldComponent],
+    exports: [FtTextFieldComponent, FtCheckBoxComponent],
     declarations: [FtTextFieldComponent],
     providers: [],
 })
