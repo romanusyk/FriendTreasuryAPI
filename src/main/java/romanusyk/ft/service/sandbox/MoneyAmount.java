0cd08687689aee037b2d12861063042a08393fb1
Roman Usyk <romanysik@gmail.com>
2018-07-28 14:48:39 +0300

    add optimizer for new model

---
+++ src/main/java/romanusyk/ft/service/sandbox/MoneyAmount.java
@@ -0,0 +1,17 @@
+package romanusyk.ft.service.sandbox;
+
+/**
+ * Created by Roman Usyk on 28.07.18.
+ */
+public class MoneyAmount {
+
+    public String user;
+    public int amount;
+
+    public MoneyAmount(String user, int amount) {
+
+        this.user = user;
+        this.amount = amount;
+    }
+
+}
