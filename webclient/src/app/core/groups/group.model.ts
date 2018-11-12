1f507239bae1fa7ef3ccf7a796112a0320358204
Roman Usyk <romanysik@gmail.com>
2018-11-12 11:50:05 +0200

    add group userDebt

--- webclient/src/app/core/groups/group.model.ts
+++ webclient/src/app/core/groups/group.model.ts
@@ -3,22 +3,19 @@ import { User } from '../users/user.model';
 export class Group {
   id: number;
   title: string;
-  amount?: number;
+  userDebt?: number;
   name?: string;
   usersCount?: number;
-  constructor(group?: Group, amount?: number) {
+  constructor(group?: Group) {
     if (!!group) {
       Object.assign(this, group);
-    }
-    if (!!amount) {
-      this.amount = amount;
     }
   }
 }
 
 export class EditGroupModel extends Group {
   isChanged?: boolean;
-  constructor(group?: Group, amount?: number) {
-    super(group, amount);
+  constructor(group?: Group) {
+    super(group);
   }
 }
