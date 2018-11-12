b45b1a5e58bd641a59f36e5444662eb584198957
Ubuntu <yevhenii@yevhenii-vm.hvcpitw5lfiedoxdl4mhfv1p5h.bx.internal.cloudapp.net>
2018-11-12 13:43:39 +0000

    add second docker-compose.yml

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
+# Create volume
+# Run this only for first time
 mkdir ~/database
-docker-compose up --build
+
+# Run only main frontend version
+docker-compose up -d
+
+# Run both old and new frontends
+# New will be available on port :8008
+docker-compose -f docker-compose.yaml -f docker-compose-new-frontend.yaml up -d
 
