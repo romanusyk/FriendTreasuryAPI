fd700321318bca7df78c6baec95d1140f1774b6c
Ubuntu <yevhenii@yevhenii-vm.hvcpitw5lfiedoxdl4mhfv1p5h.bx.internal.cloudapp.net>
2018-11-12 13:51:49 +0000

    add clone link

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,10 @@ mkdir ~/database
 docker-compose up -d
 
 # Run both old and new frontends
+# First clone this repo to home dir:
+git clone git@github.com:YevheniiStepaniuk/ft-web-client.git
+
+# Then run this
 # New will be available on port :8008
 docker-compose -f docker-compose.yaml -f docker-compose-new-frontend.yaml up -d
 
