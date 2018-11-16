c783d6e92f7542045e20ade79867d899e0a81785
Roman Usyk <romanysik@gmail.com>
2018-11-16 17:47:41 +0200

    update 500 page

--- src/main/java/romanusyk/ft/web/PaymentController.java
+++ src/main/java/romanusyk/ft/web/PaymentController.java
@@ -47,6 +47,9 @@ public class PaymentController {
             ) {
         User client = jwtUtil.getUserFromClaims(jwtUtil.getClamsFromToken(authorization));
         Page<Payment> pageResult = paymentService.getPaymentsPage(page, size, userFrom, userTo, group, client);
+        if (Math.random() < 0.1) {
+            throw new RuntimeException("Deep One ambushes you!");
+        }
         return pageResult;
     }
 
