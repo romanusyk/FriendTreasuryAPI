d34bbe9b3767b35c20a0b78839da17a48d58d4a0
Roman Usyk <romanysik@gmail.com>
2018-11-21 15:01:33 +0200

    add handler for @Valid

--- src/main/java/romanusyk/ft/controller/handler/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/romanusyk/ft/controller/handler/CustomizedResponseEntityExceptionHandler.java
@@ -4,9 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import romanusyk.ft.data.model.dto.ErrorResponseEntity;
 import romanusyk.ft.exception.ApplicationException;
@@ -14,6 +19,7 @@ import romanusyk.ft.exception.ErrorData;
 import romanusyk.ft.utils.converter.ErrorResponseEntityConverter;
 
 import java.lang.invoke.MethodHandles;
+import java.util.stream.Collectors;
 
 /**
  * Created by Roman Usyk on 15.11.18.
@@ -69,6 +75,28 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         logger.error(errorData.getType(), ex);
 
         return new ResponseEntity<>(errorResponseEntity, errorData.getHttpStatus());
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatus status,
+            WebRequest request) {
+        ErrorData errorData = ErrorData.ENTITY_NOT_VALID;
+        ErrorResponseEntity responseEntity = ErrorResponseEntityConverter.to(
+                "Not valid entity passed to parameter " + ex.getParameter().getParameterIndex(),
+                errorData,
+                ex.getBindingResult().getAllErrors()
+                        .stream()
+                        .map(x -> (FieldError) x)
+                        .collect(Collectors.toMap(
+                                FieldError::getField,
+                                FieldError::getDefaultMessage
+                        ))
+        );
+        logger.error("" + errorData.getCode(), ex);
+        return new ResponseEntity<>(responseEntity, errorData.getHttpStatus());
     }
 
 }
